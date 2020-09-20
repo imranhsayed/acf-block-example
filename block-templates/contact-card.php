efe51576471d6a270b28459e1b67ad61ae7c4985
Imran <imranhsayed@gmail.com>
2020-09-20 14:21:13 +0530

    Add block id

--- block-templates/contact-card.php
+++ block-templates/contact-card.php
@@ -1,31 +1,43 @@
 <?php
+
 $template = [
-	['core/heading', [
-		'level' => 6,
-		'content' => '<strong>Title</strong>',
-		'align' => 'center',
-		'className' => 'card-heading'
-	]],
-	[  'core/paragraph', [ 'content' => 'Description', 'align' => 'center' ] ],
-	[  'core/button', [ 'content' => 'Get in touch', 'align' => 'center' ] ],
+	[
+		'core/heading',
+		[
+			'level'     => 6,
+			'content'   => '<strong>Title</strong>',
+			'align'     => 'center',
+			'className' => 'card-heading'
+		]
+	],
+	[ 'core/paragraph', [ 'content' => 'Description', 'align' => 'center' ] ],
+	[ 'core/button', [ 'content' => 'Get in touch', 'align' => 'center' ] ],
 	[ 'core/social-links', [ 'align' => 'center' ] ],
 ];
 
-$allowed_blocks = ['core/heading', 'core/paragraph', 'core/social-links', 'core/button'];
+$allowed_blocks = [ 'core/heading', 'core/paragraph', 'core/button', 'core/social-links' ];
 
-$classes = ['block-about'];
-// Create class attribute allowing for custom "className" and "align" values.
-$classes = '';
-if( !empty($block['className']) ) {
-	$classes .= sprintf( ' %s', $block['className'] );
-}
+$classes = [ 'block-contact-card' ];
+
+// Create class attribute allowing for custom "className" values.
+$classes = ( ! empty( $block['className'] ) ) ?
+	sprintf( 'contact-card %s', $block['className'] ) :
+	'contact-card';
+
+$id = 'contact-card-' . $block['id'];
+
 ?>
-<div class="contact-card <?php echo esc_attr($classes); ?>">
-	<div class="photo"><?php echo wp_get_attachment_image( get_field( 'photo' ), 'be_thumbnail_l' ); ?></div>
-	<div class="banner"><?php echo wp_get_attachment_image( get_field( 'banner' ), 'be_thumbnail_l' ); ?></div>
+<div id=<?php echo esc_attr( $id ) ?> class="<?php echo esc_attr( $classes ); ?>">
+	<div class="photo">
+		<?php echo wp_kses_post( wp_get_attachment_image( get_field( 'photo' ) ) ); ?>
+	</div>
+	<div class="banner">
+		<?php echo wp_kses_post( wp_get_attachment_image( get_field( 'banner' ) ) ); ?>
+	</div>
 	<InnerBlocks
-		template="<?php echo esc_attr( wp_json_encode( $template ) ); ?>"
-		allowedBlocks="<?php echo esc_attr( wp_json_encode( $allowed_blocks ) ); ?>"
-		templateLock="all"
+			template="<?php echo esc_attr( wp_json_encode( $template ) ); ?>"
+			allowedBlocks="<?php echo esc_attr( wp_json_encode( $allowed_blocks ) ); ?>"
+			templateLock="all"
 	/>
+
 </div>
